1df13f5dd3673b5fffe37c4b334ea8756c7674fb
wlsdud2194 <wlsdud2194@gmail.com>
2023-01-04 01:55:51 +0900

    fix: reshape enum field type & mapping

--- test/migrateCaseConventions.test.ts
+++ test/migrateCaseConventions.test.ts
@@ -1,4 +1,4 @@
-const { camelCase, pascalCase } = require('change-case');
+const { camelCase, pascalCase, snakeCase } = require('change-case');
 const { migrateCaseConventions } = require('../src/migrateCaseConventions');
 
 test('it can map model columns with under_scores to camelCase', () => {
@@ -46,4 +46,31 @@ test('it can map relations with cascading deletion rules & foreign_key names', (
   const [result, err] = migrateCaseConventions(file_contents, pascalCase, camelCase);
   expect(err).toBeFalsy();
   expect(result.includes('@relation(fields: [projectId], references: [id], onDelete: Cascade, onUpdate: NoAction, map: "jira_issues_projects_fkey")')).toBeTruthy();
+});
+
+test('it can map enum column to enum definition', () => {
+  const file_contents = `datasource db {
+    provider = "mysql"
+    url      = env("DATABASE_URL")
+  }
+  
+  generator client {
+    provider = "prisma-client-js"
+  }
+  
+  model posts {
+    id        Int           @id @default(autoincrement())
+    content   String?       @db.VarChar
+    postType  post_type
+  }
+
+  enum post_type {
+    Note
+    Question
+  }
+  `;
+  const [result, err] = migrateCaseConventions(file_contents, pascalCase, snakeCase);
+  expect(err).toBeFalsy();
+  expect(result.includes(`post_type  PostType @map("postType")`)).toBeTruthy();
+  expect(result.includes(`enum PostType {`)).toBeTruthy();
 });
